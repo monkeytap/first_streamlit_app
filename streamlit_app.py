8f451a06be96c7f6f59c7ec6f9c8e0c9a7aa1f01
monkeytap <nate.vernon@gmail.com>
2022-09-22 11:00:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ except URLError as e:
 #SNOWFLAKE SECTION----------------------
 #import snowflake.connector
 
-def insert_row_snowflake(new_fruit)
+def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
                 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
                 return "Thanks for adding " + new_fruit
