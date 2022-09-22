1495082cb59e14a813e613cbe48aa0d466bc6ee8
monkeytap <nate.vernon@gmail.com>
2022-09-22 10:59:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,25 +54,41 @@ except URLError as e:
 #SNOWFLAKE SECTION----------------------
 #import snowflake.connector
 
+def insert_row_snowflake(new_fruit)
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+                return "Thanks for adding " + new_fruit
+        
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        streamlit.text(back_from_function)
+                               
+                               
+streamlit.stop()
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#streamlit.write('Thanks for adding ', add_my_fruit)
+
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
-streamlit.header("The fruit load list contains:")
+
+
+#streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursur() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
+#def get_fruit_load_list():
+    #with my_cnx.cursur() as my_cur:
+       #  my_cur.execute("select * from fruit_load_list")
+      #   return my_cur.fetchall()
    
 #add a button to load the fruit        
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)              
-streamlit.stop()
+#if streamlit.button('Get Fruit Load List'):
+   # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   # my_data_rows = get_fruit_load_list()
+   # streamlit.dataframe(my_data_rows)              
+
 
                
         
